75e33ee97518015ac00853033422b7814de1ee23
aodaGit <aoda2826@qq.com>
2023-04-24 19:04:49 +0800

    更新文件结构

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import initSwagger from './core/swagger';
+import initSwagger from './core/apiDoc';
 import helmet from 'helmet';
 import * as csurf from 'csurf';
 import { ConfigService } from '@nestjs/config';
--- src/modules/Address/address.controller.ts
+++ src/modules/Address/address.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UsePipes } from '@nestjs/common';
 import { ServiceEnum } from 'src/constants/service';
 import { AddressService } from './address.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import TestPipes from '../../core/pipes/testPipe';
 
 @ApiTags('地址')
+@UsePipes(new TestPipes())
 @Controller(ServiceEnum.ADDRESS)
 export class AddressController {
   constructor(private addressService: AddressService) {}
@@ -11,6 +13,8 @@ export class AddressController {
   @ApiOperation({ summary: '获取文章详情' })
   @Get('detail/:id')
   getDetail(@Param() param: { id: number }) {
+    console.log('id',param?.id);
+    
     return this.addressService.getDetail(param?.id);
   }
 }
