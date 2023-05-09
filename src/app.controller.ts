3bec2c70796bb1c5c9fff684d18681a392eec813
aodaGit <aoda2826@qq.com>
2023-05-09 10:43:30 +0800

    ceshi

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,11 +3,11 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-    constructor(pr
-      ivate readonly appService: AppService) {}
+    constructor(private readonly appService: AppService) {}
 
     @Get()
     getHello(): string {
-        return this.appService.getHello();
+        return this.appS;
+        ervice.getHello();
     }
 }
